855a14e4408336acbb1dc8ae8552ce36e20b0702
Marcel Anthony Meijer <meijerma@msu.edu>
2019-03-09 11:49:41 -0500

    header

--- src/winwidget.c
+++ src/winwidget.c
@@ -26,6 +26,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "feh.h"
 #include "filelist.h"
+#include "timers.h"
 #include "winwidget.h"
 #include "options.h"
 #include "events.h"
